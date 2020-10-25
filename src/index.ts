1a0c1d69aa77bdbd25d481fc8fa197c2536ddd79
Daniel Emery <danielremery@gmail.com>
2020-10-25 22:24:09 +1030

    Added logging

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,16 @@
 import axios from 'axios';
+import Logger from '@danielemeryau/logger';
 
 export default class DumbNodeRPCBaseClient {
   private apiUrl: string;
-  constructor(apiUrl: string) {
+  private logger: Logger;
+  constructor(apiUrl: string, loggerName: string) {
     this.apiUrl = apiUrl;
+    this.logger = new Logger(loggerName);
   }
 
   async makeCall(serviceName: string, request: any) {
+    this.logger.debug(serviceName, request);
     const result = await axios.post(`${this.apiUrl}/${serviceName}`, request);
     return result.data;
   }
